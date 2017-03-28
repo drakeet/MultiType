22236516eef859fdfd0d6d13c3b2086f92204a40
drakeet <drakeet.me@gmail.com>
2017-03-28 21:48:56 +0800

    Supplied some author tags

--- sample/src/androidTest/java/me/drakeet/multitype/sample/bilibili/BilibiliActivityDataSetADCTest.java
+++ sample/src/androidTest/java/me/drakeet/multitype/sample/bilibili/BilibiliActivityDataSetADCTest.java
@@ -32,6 +32,9 @@ import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static me.drakeet.multitype.sample.RecyclerViewMatcher.withRecyclerView;
 
+/**
+ * @author drakeet
+ */
 @RunWith(AndroidJUnit4.class)
 public class BilibiliActivityDataSetADCTest {
 
--- sample/src/androidTest/java/me/drakeet/multitype/sample/bilibili/SmokeTest.java
+++ sample/src/androidTest/java/me/drakeet/multitype/sample/bilibili/SmokeTest.java
@@ -34,6 +34,9 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
 
+/**
+ * @author drakeet
+ */
 @LargeTest
 @RunWith(AndroidJUnit4.class)
 public class SmokeTest {
