80d1330d813f39353d3bf7586d749f9a8df8bf66
drakeet <drakeet.me@gmail.com>
2017-06-12 10:21:25 +0800

    [sample] Small fix

--- sample/src/main/java/me/drakeet/multitype/sample/bilibili/BilibiliActivity.java
+++ sample/src/main/java/me/drakeet/multitype/sample/bilibili/BilibiliActivity.java
@@ -43,12 +43,12 @@ public class BilibiliActivity extends MenuBaseActivity {
 
     private static class JsonData {
 
-        private final String prefix = "这是一条长长的达到两行的标题文字";
+        private static final String PREFIX = "这是一条长长的达到两行的标题文字";
 
-        private Post post00 = new Post(R.drawable.img_00, prefix + "post00");
-        private Post post01 = new Post(R.drawable.img_01, prefix + "post01");
-        private Post post10 = new Post(R.drawable.img_10, prefix + "post10");
-        private Post post11 = new Post(R.drawable.img_11, prefix + "post11");
+        private Post post00 = new Post(R.drawable.img_00, PREFIX + "post00");
+        private Post post01 = new Post(R.drawable.img_01, PREFIX + "post01");
+        private Post post10 = new Post(R.drawable.img_10, PREFIX + "post10");
+        private Post post11 = new Post(R.drawable.img_11, PREFIX + "post11");
 
         Category category0 = new Category("title0");
         Post[] postArray = { post00, post01, post10, post11 };
--- sample/src/main/java/me/drakeet/multitype/sample/normal/TextItem.java
+++ sample/src/main/java/me/drakeet/multitype/sample/normal/TextItem.java
@@ -18,6 +18,7 @@ package me.drakeet.multitype.sample.normal;
 
 import android.support.annotation.NonNull;
 import com.google.gson.Gson;
+import java.nio.charset.Charset;
 import me.drakeet.multitype.sample.Savable;
 
 /**
@@ -40,17 +41,20 @@ public class TextItem implements Savable {
 
     @Override
     public void init(@NonNull byte[] data) {
-        String json = new String(data);
+        String json = new String(data, UTF_8);
         this.text = new Gson().fromJson(json, TextItem.class).text;
     }
 
 
     @NonNull @Override
     public byte[] toBytes() {
-        return new Gson().toJson(this).getBytes();
+        return new Gson().toJson(this).getBytes(UTF_8);
     }
 
 
     @NonNull @Override
     public String describe() { return "Text"; }
+
+
+    private static final Charset UTF_8 = Charset.forName("UTF-8");
 }
