2f49d97bc2af53bf7635ef4ade028b9295f995ba
drakeet <drakeet.me@gmail.com>
2017-06-27 14:10:57 +0800

    [sample] Added loading animation demo to TextItemView (#149)

--- sample/src/main/java/me/drakeet/multitype/sample/normal/TextItemViewBinder.java
+++ sample/src/main/java/me/drakeet/multitype/sample/normal/TextItemViewBinder.java
@@ -21,6 +21,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 import me.drakeet.multitype.ItemViewBinder;
 import me.drakeet.multitype.sample.R;
@@ -30,6 +32,9 @@ import me.drakeet.multitype.sample.R;
  */
 public class TextItemViewBinder
     extends ItemViewBinder<TextItem, TextItemViewBinder.TextHolder> {
+
+    private int lastShownAnimationPosition;
+
 
     static class TextHolder extends RecyclerView.ViewHolder {
 
@@ -54,5 +59,24 @@ public class TextItemViewBinder
     @Override
     protected void onBindViewHolder(@NonNull TextHolder holder, @NonNull TextItem textItem) {
         holder.text.setText("hello: " + textItem.text);
+
+        // should show animation, ref: https://github.com/drakeet/MultiType/issues/149
+        setAnimation(holder.itemView, holder.getAdapterPosition());
+    }
+
+
+    private void setAnimation(@NonNull View viewToAnimate, int position) {
+        if (position > lastShownAnimationPosition) {
+            Animation animation = AnimationUtils.loadAnimation(viewToAnimate.getContext(),
+                android.R.anim.slide_in_left);
+            viewToAnimate.startAnimation(animation);
+            lastShownAnimationPosition = position;
+        }
+    }
+
+
+    @Override
+    public void onViewDetachedFromWindow(@NonNull TextHolder holder) {
+        holder.itemView.clearAnimation();
     }
 }
