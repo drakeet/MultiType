6ced9eb5521d2daf020c218526c936ffaa4ccd3c
shawnlinboy <ishawn.lin@gmail.com>
2018-07-15 01:22:32 +0800

    fix a engligh grammar mistake in BinderNotFoundException.java for better intelligibility

--- library/src/main/java/me/drakeet/multitype/BinderNotFoundException.java
+++ library/src/main/java/me/drakeet/multitype/BinderNotFoundException.java
@@ -24,7 +24,7 @@ import android.support.annotation.NonNull;
 class BinderNotFoundException extends RuntimeException {
 
   BinderNotFoundException(@NonNull Class<?> clazz) {
-    super("Do you have registered {className}.class to the binder in the adapter/pool?"
+    super("Have you registered {className}.class to the binder in the adapter/pool?"
         .replace("{className}", clazz.getSimpleName()));
   }
 }
