9cc034777ab8ebf1f753df1e53335fa2b7629e87
Drakeet <drakeet@drakeet.com>
2021-05-23 20:12:07 +0800

    Remove layoutPosition & adapterPosition for ViewDelegate
    Resolve & close #315

--- library/src/main/kotlin/com/drakeet/multitype/ViewDelegate.kt
+++ library/src/main/kotlin/com/drakeet/multitype/ViewDelegate.kt
@@ -21,7 +21,7 @@ abstract class ViewDelegate<T, V : View> : ItemViewDelegate<T, ViewDelegate.Hold
     return onCreateView(context)
   }
 
-  // Override this function if you need a ViewHolder
+  // Override this function if you need a ViewHolder or positions
   open fun onBindView(holder: Holder<V>, view: V, item: T) {
     onBindView(view, item)
   }
@@ -33,23 +33,4 @@ abstract class ViewDelegate<T, V : View> : ItemViewDelegate<T, ViewDelegate.Hold
   override fun onBindViewHolder(holder: Holder<V>, item: T) = onBindView(holder, holder.view, item)
 
   class Holder<V : View>(val view: V) : RecyclerView.ViewHolder(view)
-
-  /**
-   * Warning: this property can only get the correct value in an item root view.
-   * @see RecyclerView.LayoutParams.getViewLayoutPosition
-   */
-  protected val View.layoutPosition get() = recyclerLayoutParams.viewLayoutPosition
-
-  /**
-   * Warning: this property can only get the correct value in an item root view.
-   * @see RecyclerView.LayoutParams.getViewAdapterPosition
-   */
-  protected val View.adapterPosition get() = recyclerLayoutParams.viewAdapterPosition
-
-  /**
-   * RecyclerView will automatically convert any original type of LayoutParam into [RecyclerView.LayoutParams],
-   * so for the item root view, its final LayoutParam must be [RecyclerView.LayoutParams].
-   * @see RecyclerView.LayoutManager.generateLayoutParams
-   */
-  private val View.recyclerLayoutParams get() = layoutParams as RecyclerView.LayoutParams
 }
