4183417b423870c3b81972f01c481845dede8e2e
Drakeet <drakeet@drakeet.com>
2021-06-20 18:33:45 +0800

    Merge ViewHolderDelegate & ViewHolderInflater files

--- library/src/main/kotlin/com/drakeet/multitype/ViewHolderInflater.kt
+++ library/src/main/kotlin/com/drakeet/multitype/ViewHolderInflater.kt
@@ -1,8 +1,26 @@
 package com.drakeet.multitype
 
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 /**
- * @author Drakeet Xu
+ * Alias of the [ItemViewBinder]
+ * @since 4.3.0
  */
 abstract class ViewHolderInflater<T, VH : RecyclerView.ViewHolder> : ItemViewBinder<T, VH>()
+
+/**
+ * This is a compatible version of [ItemViewDelegate].
+ * @see ItemViewDelegate
+ * @author Drakeet Xu
+ */
+abstract class ItemViewBinder<T, VH : RecyclerView.ViewHolder> : ItemViewDelegate<T, VH>() {
+
+  final override fun onCreateViewHolder(context: Context, parent: ViewGroup): VH {
+    return onCreateViewHolder(LayoutInflater.from(context), parent)
+  }
+
+  abstract fun onCreateViewHolder(inflater: LayoutInflater, parent: ViewGroup): VH
+}
